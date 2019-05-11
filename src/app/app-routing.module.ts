8b7bd47545163b9774bfa2894d242a710e1365ec
Lena <elena.borisenko1991@gmail.com>
2019-05-11 11:26:23 +0000

    add routing to the app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailsComponent } from './movies-list/movie-details/movie-details.component';
+
+const routes: Routes = [
+  { path: 'movies', component: MoviesListComponent },
+  { path: 'movie', component: MovieDetailsComponent },
+  {path: '', redirectTo: 'movies', pathMatch : 'full'}
+  
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MovieDetailsComponent } from './movies-list/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    MovieDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
